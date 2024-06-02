5b9878a033cc0bbc28c7c32fcb0e3639eb44a0bf
Sergio Cid <sergio.g.cid.m@gmail.com>
2024-06-02 22:11:09 +0200

    adding build:ui and deploy:full in package.json

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.static('dist'))
 
+//para que muestr informacion de la api
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
