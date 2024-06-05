04fa08fbcff4a0ac279223b54f84a1b40641cf8e
Sergio Cid <sergio.g.cid.m@gmail.com>
2024-06-05 19:33:44 +0200

    adding the last functions to modify the notes

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ require('dotenv').config()
 
 const Note = require('./models/note')
 
-let notes = [
-]
-
+// Middleware para servir archivos estáticos desde el directorio 'dist'
 app.use(express.static('dist'))
 
+// Middleware para habilitar CORS
+const cors = require('cors')
+app.use(cors())
+
+// Middleware para analizar solicitudes JSON
+app.use(express.json())
+
+// Middleware para registrar solicitudes
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -16,18 +22,9 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
-
-const cors = require('cors')
-
-app.use(cors())
-
-app.use(express.json())
 app.use(requestLogger)
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
+// Rutas
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -55,17 +52,31 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end()
-    }
-  }).catch(error => {
-    console.log(error);
-    response.status(500).end();
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+// Nueva ruta para actualizar la importancia de una nota
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -75,7 +86,21 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+// Middleware para manejar rutas desconocidas
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
+
+// Middleware para manejar errores
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
